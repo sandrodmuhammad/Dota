5303dea614c59eacb12caa448865ab15e94995bb
Sandro <9c.sandro@gmail.com>
2016-09-10 20:53:42 +0700

    code 2

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/MainActivity.java
@@ -2,6 +2,7 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3022.dota;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -21,5 +22,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rbCR = (RadioButton) findViewById(R.id.radioButtonCR);
+        rbSP = (RadioButton) findViewById(R.id.radioButtonSP);
+        kd = (CheckBox) findViewById(R.id.checkBox2000);
+        db = (CheckBox) findViewById(R.id.checkBox2000x);
+        lt = (CheckBox) findViewById(R.id.checkBox3000);
+        bv = (Button) findViewById(R.id.validasi);
+        etNick = (EditText) findViewById(R.id.nick);
+        tvhasil = (TextView) findViewById(R.id.hasil);
+        bok = (Button) findViewById(R.id.ok);
+        tvhasil2 = (TextView) findViewById(R.id.hasil2);
+        tvhasil3 = (TextView) findViewById(R.id.hasil3);
+        spplay = (Spinner) findViewById(R.id.playtime);
+        tvhasil4 = (TextView) findViewById(R.id.hasil4);
+
+        bv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick2();
+            }
+        });
+
+        bok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                doProcess();
+                doClick();
+
+            }
+        });
+
     }
 }

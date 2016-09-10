578bbba09aa75a2dabe051ba6399842671069219
Sandro <9c.sandro@gmail.com>
2016-09-10 20:42:47 +0700

    First Try

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl3022.dota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl3022.dota;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

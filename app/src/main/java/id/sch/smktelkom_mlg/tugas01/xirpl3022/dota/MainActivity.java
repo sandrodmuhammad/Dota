8bda679ff6c6dc839f9e0a54d72be4f5711a4e4c
Sandro <9c.sandro@gmail.com>
2016-09-10 20:53:06 +0700

    code 1

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl3022/dota/MainActivity.java
@@ -2,8 +2,20 @@ package id.sch.smktelkom_mlg.tugas01.xirpl3022.dota;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNick;
+    TextView tvhasil, tvhasil2, tvhasil3, tvhasil4;
+    Button bok, bv;
+    RadioButton rbCR, rbSP;
+    Spinner spplay;
+    CheckBox kd, db, lt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
